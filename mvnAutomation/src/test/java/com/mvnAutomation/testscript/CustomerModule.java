b4cfca25dabdc1cd77df6ea5e838cb8f61117956
User <User@DESKTOP-1SP5EH3>
2023-04-19 18:30:01 +0530

    dsrgerg

---
+++ mvnAutomation/src/test/java/com/mvnAutomation/testscript/CustomerModule.java
@@ -0,0 +1,11 @@
+package com.mvnAutomation.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void CreateCustomer() {
+	Reporter.log("createcustomer",true);
+}
+}
